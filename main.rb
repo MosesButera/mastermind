653e8cffe70e47a49eb1ab5ab661e35130f5fc7c
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-11-26 10:57:07 -0700

    Prompt for game mode selection Created menu asking player to enter "1" for code breaker or "2" for code maker role.

--- main.rb
+++ main.rb
@@ -5,3 +5,9 @@ puts "\nPlease enter player name"
 player_name = gets.chomp
 puts "\n#{player_name} please enter 1 for code breaker or 2 code maker to start game"
 choice = gets.chomp
+
+if choice.nil? || choice != '1' || choice != '2' || !choice.is_a?(String)
+  'Please enter valid choice: 1 for code breaker or 2 for code maker'
+else
+  choice = gets.chomp
+end
