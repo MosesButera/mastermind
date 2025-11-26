4b74a54b09dac415846972b21b6d65d332645df7
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-11-26 09:31:10 -0700

    add get_feedback method (black peg logic) Introduce get_feedback(guess, secret_code) and implement the black-peg (exact match) counting. Black denote correct color and correct position. Used deep copies (Marshal) so we can mark used elmenents without mutating the original arrays, ensuring later steps (white-peg logic) are accurate.

--- lib/computer.rb
+++ lib/computer.rb
@@ -15,4 +15,24 @@ class Computer
     six_possible_numbers = [1, 2, 3, 4, 5, 6]
     six_possible_numbers.repeated_permutation(4).to_a
   end
+
+  # Method: get_feedback(secret, guess)
+  def get_feedback(guess, secret_code)
+    # Initialize deep copies that will track changes in guess and secret_code
+    guess_copy = Marshal.load(Marshal.dump(guess))
+    secret_code_copy = Marshal.load(Marshal.dump(secret_code))
+    # initialize counters
+    black_peg = 0
+    white_peg = 0
+
+    # Black pegs (exact matches)
+    guess_copy.each_with_index do |element, index|
+      next unless element == secret_code[index]
+
+      black_peg += 1
+      index = secret_code_copy.index(element)
+      secret_code_copy[index] = nil # to avoid double counting in white peg mark
+      # positions as used by setting to null.
+    end
+  end
 end
