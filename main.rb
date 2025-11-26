6d927dfcff7cc90b252bd218a31ae23f2524627f
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-11-26 10:55:24 -0700

    Display welcome message and get player name Added game start message and prompt that captures player name input using gets.chomp. This personalizes the game experience and establishes friendly user interaction from the beginning.

--- main.rb
+++ main.rb
@@ -1,1 +1,7 @@
 require_relative 'lib/computer'
+
+puts 'Game Start :)'
+puts "\nPlease enter player name"
+player_name = gets.chomp
+puts "\n#{player_name} please enter 1 for code breaker or 2 code maker to start game"
+choice = gets.chomp
