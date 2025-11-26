d9202ec42b819378b31d3358875348c284b530f4
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-11-26 09:21:02 -0700

    Add method to generate random secret code Implement create_secret_code method that randomly selects one code from all possible 1296 combinations. This provides the game's secret code when computer acts as code maker. Uses sampling for true randomness.

--- lib/computer.rb
+++ lib/computer.rb
@@ -3,4 +3,10 @@ class Computer
   def initialize(name)
     @name = name
   end
+
+  # Generate random number array from the 1296 set of four possible arrays.
+  def create_secret_code
+    all_codes = create_all_codes
+    all_codes.sample
+  end
 end
